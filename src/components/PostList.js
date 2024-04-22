784a2765ebd7f6d91b61a5673f3bfd03f27b5694
Darylcosm0 <mutaidaryl@gmail.com>
2024-04-22 09:54:10 +0300

    Displayed dummy posts in PostList component

---
+++ src/components/PostList.js
@@ -0,0 +1,18 @@
+// src/components/PostList.js
+import React from "react";
+import posts from "../data/posts";
+
+const PostList = () => {
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
