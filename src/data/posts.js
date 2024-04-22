fb6361d4aa16e7357ed231ac649876cdd15b3f5e
Darylcosm0 <mutaidaryl@gmail.com>
2024-04-22 09:47:24 +0300

    Added dummy data for blog posts

---
+++ src/data/posts.js
@@ -0,0 +1,17 @@
+//added dummy data 
+const posts = [
+    {
+      id: 1,
+      title: "First Post",
+      content: "This is the content of the first post.",
+    },
+    {
+      id: 2,
+      title: "Second Post",
+      content: "This is the content of the second post.",
+    },
+    
+  ];
+  
+  export default posts;
+  
